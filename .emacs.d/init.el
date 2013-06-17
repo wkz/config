2c6092662c6dcf378a6eed2ecc27c6ffe244ab1a
Tobias Waldekranz <tobias@waldekranz.com>
2013-06-17 07:11:21 +0200

    window borders, xmobar, misc

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,6 +6,7 @@
 (package-initialize)
 
 ;; loads
+(require 'uniquify)
 (require 'tramp)
 
 ;; UI
@@ -14,7 +15,24 @@
 (tool-bar-mode -1)
 (toggle-scroll-bar -1)
 (load-theme 'solarized-dark t)
+(setq uniquify-buffer-name-style 'post-forward)
 
 ;; Keys
 (global-set-key (kbd "M-<tab>") 'dabbrev-expand)
 (global-set-key (kbd "M-r") 'rgrep)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(background-color "#7f7f7f")
+ '(background-mode dark)
+ '(cursor-color "#5c5cff")
+ '(custom-safe-themes (quote ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
+ '(foreground-color "#5c5cff"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,16 +1,27 @@
 import XMonad
+import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers (isFullscreen, doFullFloat)
+import XMonad.Util.Run (spawnPipe)
 
 myManageHook :: ManageHook
 myManageHook = composeAll
 	     [ isFullscreen --> doFullFloat
 	     ]
 
-main = xmonad defaultConfig
-     { modMask = mod4Mask
-     , manageHook = manageHook defaultConfig <+> myManageHook
+main = do
+xmproc <- spawnPipe "/usr/bin/xmobar /home/wkz/.xmobarrc"
+bgproc <- spawnPipe "/usr/bin/xsetroot -solid '#002630'"
+typroc <- spawnPipe "trayer --edge top --align right --widthtype request --expand true --SetDockType true --SetPartialStrut true --transparent true --alpha 0 --tint 0x002630 --expand true --heighttype pixel --height 18 --monitor 0 --padding 0"
+volproc <- spawnPipe "gnome-sound-applet"
+nmproc <- spawnPipe "nm-applet"
 
-     , normalBorderColor = "black"
-     , focusedBorderColor = "#536878"
+xmonad $ defaultConfig
+     { modMask = mod4Mask
+     , manageHook = manageDocks <+> manageHook defaultConfig <+> myManageHook
+     , layoutHook = avoidStruts $ layoutHook defaultConfig
+
+     , borderWidth = 5
+     , normalBorderColor = "#002630"
+     , focusedBorderColor = "#073642"
      }
 
